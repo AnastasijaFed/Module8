d87814dbe0c27d1961a6ae177332671362ced9d1
Anastasija <nastefedorenko@gmail.com>
2022-03-22 08:59:30 +0200

    Final project 8th module

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        File f1 = new File("C:\\Final8", "Text1.txt");
+        File f2 = new File("C:\\Final8", "Text2.txt");
+       boolean doesExist1 = f1.exists();
+       boolean doesExist2 = f2.exists();
+        double f1Size = f1.length();
+        double f2Size = f2.length();
+       if(doesExist1 == true) System.out.println("File's " +f1.getName()+ " size is " +f1Size+ " bytes");
+       else System.out.println("File " +f1.getName()+ " doesn't exist");
+        if(doesExist2 == true) System.out.println("File's " +f2.getName()+ " size is " +f2Size+ " bytes");
+        else System.out.println("File " +f2.getName()+ " doesn't exist");
+    }
+
+}
